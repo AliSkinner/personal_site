336c0f985d970f44a3479b3888465261d5ad0cfd
alistair.skinner <alistair@thepetspyjamas.com>
2016-01-07 11:18:28 +0000

    connected up skeleton homepage

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'personal_site.views.home', name='home'),
+    url(r'^$', 'homepage.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
