2dffea536deb373e3fdfa6dde33306f42a414cee
Ali Skinner <awskinner26@gmail.com>
2016-01-09 14:48:47 +0000

    hooked up static files

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -88,3 +89,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = 'os.path.join(BASE_DIR, "staticfiles"),'
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
