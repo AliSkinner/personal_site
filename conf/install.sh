0ef6f9f5cde2d81f48cc6e1a6c2f0ffe921137a6
Ubuntu <ubuntu@ip-172-31-31-247.us-west-2.compute.internal>
2016-01-13 15:55:35 +0000

    nginx & supervisor conf and apt-get dependencies

---
+++ conf/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install git
+sudo apt-get -y install python-pip
+sudo apt-get -y install mysql-server 
+sudo apt-get -y install mysql-client 
+sudo apt-get -y install libmysqlclient-dev
+sudo apt-get -y install build-essential autoconf libtool pkg-config
+sudo apt-get -y install python-dev 
+sudo apt-get -y install python2.7-dev
+sudo apt-get -y install libjpeg8-dev
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install supervisor
+sudo apt-get -y install nginx
