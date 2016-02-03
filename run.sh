2b17a3ce703ad3a653848534dbc6d860ae121f61
alistair.skinner <alistair@thepetspyjamas.com>
2016-02-03 15:38:33 +0000

    fixed wsgi path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/ubuntu/.virtualenvs/personal_site/bin/gunicorn personal_site.wsgi
+/home/ubuntu/.virtualenvs/personal_site/bin/gunicorn /home/ubuntu/personal_site/personal_site.wsgi
