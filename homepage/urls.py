95d8298f8d7fcc5ea9527e7fdeed4fa77537fe55
alistair.skinner <alistair@thepetspyjamas.com>
2016-02-05 17:24:57 +0000

    switched contacts to class-based view

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
-    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^contact/$', views.ContactPageView.as_view(), name='contact'),
     url(r'^email_me/$', views.email_me, name='email_me'),
 
 
--- homepage/views.py
+++ homepage/views.py
@@ -4,7 +4,6 @@ from .models import Item
 from django.core.mail import send_mail, EmailMultiAlternatives
 from django.conf import settings
 from django.template import loader
-
 from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
@@ -15,23 +14,15 @@ class HomePageView(TemplateView):
         context['hello'] = 'world'
         return context
 
-#
-# def home(request):
-#     # to change
-#     context = {
-#                 'portrait_pic': 'portrait_pic',
-#                 'tag_line': 'tag_line',
-#             }
-#     template = loader.get_template("homepage/homepage.html")
-#
-#     return HttpResponse(template.render(context, request))
 
-def contact(request):
-    context = {}
-    return render(request, "homepage/contact.html", context)
+class ContactPageView(TemplateView):
+    template_name = "homepage/contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactPageView, self).get_context_data(**kwargs)
+        return context
 
 def email_me(request):
-
     subject = 'Email Contact from AliSkinner.com'
     body = request.POST['email-me-message']
     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, ["awskinner26@gmail.com"])
