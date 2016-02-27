f1dfb823f684ba8f54c92c6c620c0707edf6e461
Ali Skinner <awskinner26@gmail.com>
2016-02-27 12:36:51 +0000

    basic about page

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url(r'^contact/$', views.ContactPageView.as_view(), name='contact'),
     url(r'^email_me/$', views.email_me, name='email_me'),
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+
 
 
 ]
